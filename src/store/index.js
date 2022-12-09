8ab2ed5aef198bf067770be51ffbc407c3fd5c91
Barış Sönmez <baris.sonmez@hotmail.com.tr>
2022-12-10 01:08:51 +0300

    vuex store fixed

--- src/store/index.js
+++ src/store/index.js
@@ -4,17 +4,20 @@ import axios from 'axios';
 export default createStore({
   state: {
     // Endpoints of API
-    endpointLastBlogs: 'https://api.barissonmez.com.tr/api/v1/posts/6/limit',
-    endpointTags: 'https://api.barissonmez.com.tr/api/v1/tags',
-    endpointHomeReferences: 'https://api.barissonmez.com.tr/api/v1/posts/4/limit',
+    apiURL: 'https://api.barissonmez.com.tr/api/v1/',
+    apiBaseURL: 'https://api.barissonmez.com.tr/',
 
     // States
     token: 'TOKEN_WILL_BE_HERE',
     lastBlogs: [],
     tags: [],
     homeServices: [],
-    homeReferences: []
+    homeReferences: [],
+    blogPost: [],
+    servicePost: [],
+    socials: []
   },
+
   mutations: {
     _setLastBlogs(state, payload) {
       state.lastBlogs = payload;
@@ -27,12 +30,23 @@ export default createStore({
     },
     _setHomeReferences(state, payload) {
       state.homeReferences = payload;
+    },
+    _setBlogPost(state, payload) {
+      state.blogPost = payload;
+    },
+    _setServicePost(state, payload) {
+      state.servicePost = payload;
+    },
+    _setSocials(state, payload) {
+      state.socials = payload;
     }
   },
+
   actions: {
     async setLastBlogs({ commit }) {
       try {
-        const response = await axios.get(this.state.endpointLastBlogs, {
+        const url = `${this.state.apiURL}posts/6/limit`;
+        const response = await axios.get(url, {
           headers: {
             'Content-Type': 'application/json',
             Authorization: 'Bearer ' + this.state.token
@@ -48,7 +62,8 @@ export default createStore({
     },
     async setTags({ commit }) {
       try {
-        const response = await axios.get(this.state.endpointTags, {
+        const url = `${this.state.apiURL}tags`;
+        const response = await axios.get(url, {
           headers: {
             'Content-Type': 'application/json',
             Authorization: 'Bearer ' + this.state.token
@@ -66,25 +81,34 @@ export default createStore({
       commit('_setHomeServices', [
         {
           title: 'web uygulamalar',
-          url: '/hizmetler/web-uygulamalari'
+          url: '/hizmetler/web-uygulamalar'
         },
         {
           title: 'mobil uygulamalar',
-          url: '/hizmetler/mobil-uygulamalari'
+          url: '/hizmetler/mobil-uygulamalar'
         },
         {
           title: 'gomülü sistemler',
           url: '/hizmetler/gomulu-sistemler'
         },
         {
-          title: 'grafik tasarım',
-          url: '/hizmetler/grafik-tasarim'
+          title: 'UI/grafik tasarım',
+          url: '/hizmetler/ui-grafik-tasarim'
         }
+      ]);
+    },
+    setSocials({ commit }) {
+      commit('_setSocials', [
+        { name: 'github', url: 'https://github.com/sonmez-baris' },
+        { name: 'twitter', url: 'https://twitter.com/sonmezbaris' },
+        { name: 'linkedin', url: 'https://www.linkedin.com/in/barissonmez/' },
+        { name: 'medium', url: 'https://medium.com/@sonmez.baris' }
       ]);
     },
     async setHomeReferences({ commit }) {
       try {
-        const response = await axios.get(this.state.endpointHomeReferences, {
+        const url = `${this.state.apiURL}posts/4/limit`;
+        const response = await axios.get(url, {
           headers: {
             'Content-Type': 'application/json',
             Authorization: 'Bearer ' + this.state.token
@@ -98,9 +122,44 @@ export default createStore({
       } catch (err) {
         console.log(err);
       }
+    },
+    async setBlogPost({ commit }, payload) {
+      try {
+        const url = `${this.state.apiURL}posts/${payload.seflink}`;
+        const response = await axios.get(url, {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + this.state.token
+          }
+        });
+        commit('_setBlogPost', response.data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    async setServicePost({ commit }, payload) {
+      try {
+        const url = `${this.state.apiURL}posts/${payload.seflink}`;
+        const response = await axios.get(url, {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + this.state.token
+          }
+        });
+        commit('_setServicePost', response.data.data);
+      } catch (err) {
+        console.log(err);
+      }
     }
   },
+
   getters: {
+    getApiBaseURL(state) {
+      return state.apiBaseURL;
+    },
+    getApiURL(state) {
+      return state.apiURL;
+    },
     lastBlogs(state) {
       return state.lastBlogs;
     },
@@ -112,8 +171,19 @@ export default createStore({
     },
     getHomeReferences(state) {
       return state.homeReferences;
+    },
+    getBlogPost(state) {
+      return state.blogPost;
+    },
+    getServicePost(state) {
+      return state.servicePost;
+    },
+    getSocials(state) {
+      return state.socials;
     }
   },
+
   modules: {
   }
+
 });
