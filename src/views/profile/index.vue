171ab1fcefa1de208f3e41a4fbe5620cb027a988
Barış Sönmez <baris.sonmez@hotmail.com.tr>
2022-12-10 01:10:39 +0300

    about-me page completed

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,50 +1,42 @@
 <template>
-  <div class="flex px-2 py-4">
-    <div class="w-20 h-20 flex-shrink-0"><img src="@/assets/profile.jpg" class="w-20 h-20 object-cover rounded-full" alt=""></div>
-    <div class="ml-2">
-      <p class="text-sm">Merhaba, ben Barış Sönmez.<br>
-      Antalya’da yaşayan, full stack web & mobil uygulama geliştiricisiyim.</p>
-      <p class="flex flex-wrap">
-        <a href="#" class="top-contact-item">&lt;github&gt;</a>
-        <a href="#" class="top-contact-item">&lt;twitter&gt;</a>
-        <a href="#" class="top-contact-item">&lt;linkedin&gt;</a>
-        <a href="#" class="top-contact-item">&lt;instagram&gt;</a>
-      </p>
-    </div>
-  </div>
+  <AboutMe />
   <div class="flex px-2 flex-row mt-2 flex-wrap w-full">
-    <span class="skill-tag">php</span>
-    <span class="skill-tag">javascript</span>
-    <span class="skill-tag">flutter</span>
-    <span class="skill-tag">dart</span>
-    <span class="skill-tag">html</span>
-    <span class="skill-tag">css</span>
-    <span class="skill-tag">laravel</span>
-    <span class="skill-tag">codeigniter</span>
-    <span class="skill-tag">sql</span>
-    <span class="skill-tag">vue</span>
-    <span class="skill-tag">tailwind</span>
-    <span class="skill-tag">bootstrap</span>
-    <span class="skill-tag">aermod</span>
-    <span class="skill-tag">illustrator</span>
-    <span class="skill-tag">photoshop</span>
-    <span class="skill-tag">fireworks</span>
-    <span class="skill-tag">premiere</span>
-    <span class="skill-tag">kicad</span>
-    <span class="skill-tag">easyeda</span>
-    <span class="skill-tag">figma</span>
-    <span class="skill-tag">avr</span>
-    <span class="skill-tag">surfer</span>
-    <span class="skill-tag">autocad</span>
-    <span class="skill-tag">blender</span>
-    <span class="skill-tag">cinema4d</span>
-    <span class="skill-tag">netcad</span>
-    <span class="skill-tag">pwa</span>
-    <span class="skill-tag">jquery</span>
+    <Skill />
   </div>
   <div class="flex flex-col px-2 mt-4">
     <h4 class="font-semibold text-lg my-3 border-b-1">barış sönmez <span class="font-normal">kimdir?</span></h4>
-    <p class="indent-4 mb-3">Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.</p>
-    <p class="indent-4 mb-3">Yaygın inancın tersine, Lorem Ipsum rastgele sözcüklerden oluşmaz. Kökleri M.Ö. 45 tarihinden bu yana klasik Latin edebiyatına kadar uzanan 2000 yıllık bir geçmişi vardır. Virginia'daki Hampden-Sydney College'dan Latince profesörü Richard McClintock, bir Lorem Ipsum pasajında geçen ve anlaşılması en güç sözcüklerden biri olan 'consectetur' sözcüğünün klasik edebiyattaki örneklerini incelediğinde kesin bir kaynağa ulaşmıştır. Lorm Ipsum, Çiçero tarafından M.Ö. 45 tarihinde kaleme alınan "de Finibus Bonorum et Malorum" (İyi ve Kötünün Uç Sınırları) eserinin 1.10.32 ve 1.10.33 sayılı bölümlerinden gelmektedir. Bu kitap, ahlak kuramı üzerine bir tezdir ve Rönesans döneminde çok popüler olmuştur. Lorem Ipsum pasajının ilk satırı olan "Lorem ipsum dolor sit amet" 1.10.32 sayılı bölümdeki bir satırdan gelmektedir.</p>
+    <p class="indent-4 mb-3" v-html="post.content"></p>
   </div>
 </template>
+
+<script>
+import { useStore } from 'vuex';
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+import AboutMe from '@/components/AboutMe.vue';
+import Skill from '@/components/Skill.vue';
+
+export default {
+  name: 'ServiceDetailPost',
+  components: {
+    AboutMe,
+    Skill
+  },
+  setup() {
+    const store = useStore();
+    const route = useRoute();
+    const lastPartOfUrl = route.path.split('/').pop();
+
+    const setBlogPost = () => {
+      store.dispatch('setBlogPost', { seflink: lastPartOfUrl });
+    };
+    setBlogPost();
+
+    const post = computed(() => {
+      return store.getters.getBlogPost;
+    });
+
+    return { post };
+  }
+};
+</script>
